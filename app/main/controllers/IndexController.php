50eb16705bc43077128591274a6d4ee331efd322
sbrocos <sergiobrocos@gmail.com>
2013-01-17 18:25:53 +0100

    APP base para el proyecto

---
+++ app/main/controllers/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+use ICHI\IF_CONTROLLER;
+use MODELOS\Article;
+
+require_once APP_PATH .'/main/modelos/Article.php';
+
+class IndexController extends IF_CONTROLLER
+{
+
+    public function IndexAction()
+    {
+
+    }
+
+    public function AboutAction()
+    {
+
+    }
+}
---
+++ app/main/modelos/Article.php
@@ -0,0 +1,31 @@
+<?php
+namespace MODELOS;
+
+use ICHI\IF_DBCONECTION;
+use IF_DEBUG;
+
+class Article extends IF_DBCONECTION
+{
+//     public $_name;
+
+    public function __construct()
+    {
+        $this->_name = "article";
+        $this->connect();
+    }
+
+    public function listArticles()
+    {
+        $insert = $this->insert();
+        $insert->values(array('title'=>'Contacto', 'mini_description' => 'lo que toque'));
+\IF_DEBUG::dump($insert->getCode());
+        $select = $this->select();
+
+        $list = $this->run($insert);
+
+        return $list;
+    }
+
+
+
+}
